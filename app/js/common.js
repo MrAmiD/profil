ead16335dd43356f1bc5c4c2be2aa1131397670f
MrAmiD <nadymov2910@gmail.com>
2018-05-17 18:45:20 +0500

    header, slider

--- app/js/common.js
+++ app/js/common.js
@@ -11,7 +11,7 @@ function scrollAnimate(selector){
 };
 function MainSliderInit(){//слайдер на главной странице
     $('.main-slider').slick({
-        dots: false,
+        dots: true,
         infinite: true,
         //dotsClass: 'slick-dots container d-flex align-items-center justify-content-end',
         responsive: [
@@ -24,10 +24,10 @@ function MainSliderInit(){//слайдер на главной странице
             }
         ],
         prevArrow: '<button type="button" class="slick-prev sliderAboutArrow">' +
-        '<svg xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink" width="9px" height="15px"> <path fill-rule="evenodd"  fill="rgb(0, 0, 0)" d="M9.006,1.033 C8.971,1.061 8.934,1.087 8.902,1.118 C6.805,3.175 4.709,5.231 2.612,7.288 C2.580,7.319 2.549,7.352 2.508,7.395 C2.547,7.437 2.582,7.477 2.621,7.514 C4.710,9.564 6.799,11.613 8.890,13.662 C8.924,13.696 8.967,13.723 9.006,13.753 C9.006,13.783 9.006,13.812 9.006,13.842 C8.859,13.979 8.710,14.113 8.566,14.253 C8.306,14.506 8.048,14.762 7.806,15.000 C5.210,12.455 2.621,9.917 -0.005,7.343 C0.017,7.331 0.075,7.312 0.114,7.274 C2.544,4.893 4.973,2.511 7.402,0.129 C7.571,-0.037 7.972,-0.038 8.140,0.126 C8.428,0.408 8.717,0.691 9.006,0.973 C9.006,0.993 9.006,1.013 9.006,1.033 Z"/></svg>' +
+        '<svg xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink" width="19px" height="35px"><path fill-rule="evenodd" fill="rgb(231, 229, 223)" d="M-0.015,18.198 L17.418,34.995 L18.931,33.598 L2.402,17.500 L18.986,1.402 L17.409,0.004 L-0.015,16.801 L-0.015,18.198 Z"/></svg>' +
         '</button>',
         nextArrow: '<button type="button" class="slick-next sliderAboutArrow">' +
-        '<svg xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink" width="9px" height="15px"> <path fill-rule="evenodd"  fill="rgb(0, 0, 0)" d="M-0.006,13.967 C0.029,13.939 0.066,13.913 0.097,13.881 C2.195,11.825 4.291,9.769 6.388,7.712 C6.420,7.681 6.451,7.648 6.492,7.605 C6.453,7.563 6.418,7.523 6.379,7.486 C4.290,5.436 2.201,3.387 0.110,1.338 C0.076,1.304 0.033,1.277 -0.006,1.248 C-0.006,1.218 -0.006,1.188 -0.006,1.158 C0.141,1.021 0.290,0.887 0.434,0.747 C0.695,0.494 0.952,0.239 1.194,-0.000 C3.790,2.545 6.379,5.083 9.005,7.657 C8.983,7.669 8.925,7.688 8.886,7.726 C6.456,10.107 4.027,12.489 1.598,14.871 C1.429,15.037 1.028,15.038 0.860,14.874 C0.572,14.592 0.283,14.309 -0.006,14.027 C-0.006,14.007 -0.006,13.987 -0.006,13.967 Z"/></svg>' +
+        '<svg xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink" width="19px" height="35px"><path fill-rule="evenodd" fill="rgb(231, 229, 223)" d="M19.015,16.802 L1.582,0.005 L0.069,1.402 L16.598,17.500 L0.014,33.598 L1.591,34.996 L19.015,18.199 L19.015,16.802 Z"/></svg>' +
         '</button>'
     });
 
@@ -98,6 +98,31 @@ $(document).on('click', '#my-header .close-find', function () {
 
     $('.find-form').fadeOut();
 
+});
+
+
+
+$(document).on('mouseenter', '#catalog-menu .svg-ul li', function () {
+    console.log('mouseenter', $(this).index()+1);
+    $('#catalog-menu .sub-m').hide();
+    $('#catalog-menu .sub-m').eq($(this).index()).fadeIn();
+
+    $('#catalog-menu .show-c2').hide();
+
+    if($('#catalog-menu .sub-m').eq($(this).index()).data().show !== null){
+        $($('#catalog-menu .sub-m').eq($(this).index()).data().show).fadeIn();
+    }
+});
+$(document).on('mouseleave', '#catalog-menu .svg-ul li', function () {
+    //$('#catalog-menu .sub-m').eq($(this).index()).hide();
+    //console.log('mouseleave')
+});
+$(document).on('click', '.catalog-menu-btn', function () {
+    $('#catalog-menu').click();
+    $('.fancybox-is-open .fancybox-bg').css({background: '#ffffff', opacity: 0.97});
+});
+$(document).on('click', '#catalog-menu .fancybox-close-small', function () {
+    $('.fancybox-is-open .fancybox-bg').css({background: '#1e1e1e', opacity: 0.97});
 });
 
 $(function() {
